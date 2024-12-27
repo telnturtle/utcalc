9f117d0a269da11978a14eb9f905f8efef86228e
엄휘용 <telnturtle@gmail.com>
2024-12-27 15:10:53 +0900

    refactor: CurrentUnixTimestamp 컴포넌트로 기능 분리 및 App.tsx에서 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,10 @@
-import dayjs from 'dayjs'
 import './App.css'
-import { useEffect, useState } from 'react'
-import { useForceUpdate } from './util/forceUpdate'
-import { css } from '@emotion/react'
+import CurrentUnixTimestamp from './CurrentTimestamp'
 
-const appCss = {
-  h3: css`
-    font-weight: 300;
-  `,
-  p: css`
-    font-weight: bold;
-  `,
-  copied: css`
-    font-size: var(--font-size-caption);
-    &.visible {
-      visibility: visible;
-    }
-    visibility: hidden;
-  `,
-}
 function App() {
-  const currentUnixTimestamp = dayjs().unix()
-
-  const { forceUpdate } = useForceUpdate()
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      forceUpdate()
-    }, 500)
-    return () => clearInterval(interval)
-  }, [forceUpdate])
-
-  const initialCopiedText = '.'
-  const [copiedText, setCopiedText] = useState<string>(initialCopiedText)
-  const isVisibleCopiedText = copiedText !== initialCopiedText
-
-  const handleClickButton = () => {
-    navigator.clipboard.writeText(currentUnixTimestamp.toString())
-    setCopiedText(`${currentUnixTimestamp} copied!`)
-  }
-
   return (
     <>
-      <h3 css={appCss.h3}>The Current Unix Timestamp</h3>
-      <p css={appCss.p}>{currentUnixTimestamp}</p>
-      <button onClick={handleClickButton}>Copy</button>
-      <div
-        {...(isVisibleCopiedText ? { className: 'visible' } : null)}
-        css={appCss.copied}
-      >
-        {copiedText}
-      </div>
+      <CurrentUnixTimestamp />
     </>
   )
 }
---
+++ src/CurrentTimestamp.tsx
@@ -0,0 +1,57 @@
+import dayjs from 'dayjs'
+import { useEffect, useState } from 'react'
+import { useForceUpdate } from './util/forceUpdate'
+import { css } from '@emotion/react'
+
+const currentUnixTimestampCss = {
+  h3: css`
+    font-weight: 300;
+  `,
+  p: css`
+    font-weight: bold;
+  `,
+  copied: css`
+    font-size: var(--font-size-caption);
+    &.visible {
+      visibility: visible;
+    }
+    visibility: hidden;
+  `,
+}
+function CurrentUnixTimestamp() {
+  const currentUnixTimestamp = dayjs().unix()
+
+  const { forceUpdate } = useForceUpdate()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      forceUpdate()
+    }, 500)
+    return () => clearInterval(interval)
+  }, [forceUpdate])
+
+  const initialCopiedText = '.'
+  const [copiedText, setCopiedText] = useState<string>(initialCopiedText)
+  const isVisibleCopiedText = copiedText !== initialCopiedText
+
+  const handleClickButton = () => {
+    navigator.clipboard.writeText(currentUnixTimestamp.toString())
+    setCopiedText(`${currentUnixTimestamp} copied!`)
+  }
+
+  return (
+    <>
+      <h3 css={currentUnixTimestampCss.h3}>The Current Unix Timestamp</h3>
+      <p css={currentUnixTimestampCss.p}>{currentUnixTimestamp}</p>
+      <button onClick={handleClickButton}>Copy</button>
+      <div
+        {...(isVisibleCopiedText ? { className: 'visible' } : null)}
+        css={currentUnixTimestampCss.copied}
+      >
+        {copiedText}
+      </div>
+    </>
+  )
+}
+
+export default CurrentUnixTimestamp
